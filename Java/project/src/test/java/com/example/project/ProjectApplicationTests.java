75550531aba40d0a6e5a7576e9d95d74db68f3fc
Guilherme Sant'Anna <guilherme.santanna@iteris.com.br>
2020-01-23 15:28:43 -0300

    testes integrados

--- Java/project/src/test/java/com/example/project/ProjectApplicationTests.java
+++ Java/project/src/test/java/com/example/project/ProjectApplicationTests.java
@@ -2,8 +2,6 @@ package com.example.project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
